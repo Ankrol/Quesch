e1942f376a1b6d6ef1a06fea3c4bbe808b7f78c2
qinh <ankrol@163.com>
2023-03-26 12:13:38 +0800

    1.init

---
+++ src/test/java/com/quesch/QueschApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quesch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueschApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
