a37d49ecab4145b038f91d36aeded75502325465
ankrol <ailuleiya@163.com>
2023-03-28 10:55:38 +0800

    1-4 delete and recreate vue

--- quesch-vue/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import '@tabler/core/dist/css/tabler.css'
-import '@tabler/core/dist/js/tabler'
-
-createApp(App).use(store).use(router).mount('#app')
--- quesch-vue/src/views/HomeView.vue
+++ quesch-vue/src/views/HomeView.vue
@@ -1,24 +1,18 @@
 <template>
   <div class="home">
-    <a href="#" class="btn" role="button">Link</a>
-    <button class="btn">Button</button>
-    <input type="button" class="btn" value="Input" />
-    <input type="submit" class="btn" value="Submit" />
-    <input type="reset" class="btn" value="Reset" />
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-export default defineComponent({
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
   name: 'HomeView',
   components: {
-    HelloWorld,
-  },
-});
+    HelloWorld
+  }
+}
 </script>
-
-
